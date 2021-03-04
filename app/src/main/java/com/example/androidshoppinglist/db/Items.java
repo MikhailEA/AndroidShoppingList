1a0c56fda3b2c927995aa5b276d7aad1c51a4359
MikhailEA <michev.vork@gmail.com>
2021-03-04 18:47:00 +0300

    Работа продолжается

---
+++ app/src/main/java/com/example/androidshoppinglist/db/Items.java
@@ -0,0 +1,23 @@
+package com.example.androidshoppinglist.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Items {
+
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "itemName")
+    public  String itemName;
+
+    @ColumnInfo(name = "categoryId")
+    public  String categoryId;
+
+    @ColumnInfo(name = "completed")
+    public  String completed;
+
+
+}
