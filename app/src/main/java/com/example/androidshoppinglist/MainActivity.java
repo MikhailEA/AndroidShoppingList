00beb8761bd6f0e4e2f82ea9df7e810a23dd54bb
MikhailEA <michev.vork@gmail.com>
2021-03-05 18:10:24 +0300

    Добавление лейаутов

--- app/src/main/java/com/example/androidshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/androidshoppinglist/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.androidshoppinglist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Shopping List");
+
+        ImageView addNewimageView = findViewById(R.id.addNewCategoryImageView);
+        addNewimageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddCategoryDialog()
+
+            }
+        });
+    }
+
+    private void showAddCategoryDialog() {
+        AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        View dialogView = getLayoutInflater().inflate(R.layout.add_category_layout, null);
+        EditText enterCategoryInput = dialogView.findViewById(R.id.enterCategoryInput);
+        TextView createButton = dialogView.findViewById(R.id.createButton);
+        TextView cancelButton = dialogView.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = enterCategoryInput.getText().toString();
+                if (TextUtils.isEmpty(name)) {
+                    Toast.makeText(MainActivity.this, "Enter category name", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                dialogBuilder.dismiss();
+            }
+        });
     }
 }
